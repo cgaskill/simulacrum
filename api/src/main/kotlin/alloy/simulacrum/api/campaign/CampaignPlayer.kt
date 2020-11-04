dfa468366dec5858b6619d11d044bc35f8605410
Chris Gaskill <buchrisgaskill@gmail.com>
2020-11-04 00:47:50 -0600

    Updating API dependencies

--- api/src/main/kotlin/alloy/simulacrum/api/campaign/CampaignPlayer.kt
+++ api/src/main/kotlin/alloy/simulacrum/api/campaign/CampaignPlayer.kt
@@ -1,16 +1,22 @@
 package alloy.simulacrum.api.campaign
 
 import alloy.simulacrum.api.user.Users
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.jodatime.datetime
 
 object CampaignPlayers: LongIdTable() {
-    val campaign = reference("campaign_id", Campaigns).primaryKey(2)
-    val player = reference("player_id", Users).primaryKey(3)
+    val campaign = reference("campaign_id", Campaigns)
+    val player = reference("player_id", Users)
     val lastAccessed = datetime("last_accessed").nullable()
+    init {
+        uniqueIndex("CampaignPlayerUniqueConstraint", campaign, player)
+    }
 }
 
 class CampaignPlayer(id: EntityID<Long>) : LongEntity(id) {
